c688303427e177d7f6ab7ede22695eebd0501c4c
Vicente Losada <vi_losada@hotmail.com>
2020-10-15 20:13:10 -0500

    Updates

---
+++ precache-manifest.db72dc96f591dde9ef70f61623b45bd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014741a4373bde5ac737439e60f03dfb",
+    "url": "/top-content-create-react-app/index.html"
+  },
+  {
+    "revision": "71224a71878945afdaa4",
+    "url": "/top-content-create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea6ded133674653a5205",
+    "url": "/top-content-create-react-app/static/js/2.71359e7d.chunk.js"
+  },
+  {
+    "revision": "8657ef8b637c0fdd93e8773d35d3c779",
+    "url": "/top-content-create-react-app/static/js/2.71359e7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71224a71878945afdaa4",
+    "url": "/top-content-create-react-app/static/js/main.becc9c43.chunk.js"
+  },
+  {
+    "revision": "6061a5d1a8d137a03df3",
+    "url": "/top-content-create-react-app/static/js/runtime-main.2b230038.js"
+  },
+  {
+    "revision": "5f4e1a49c3991acdd56d01bb6060c815",
+    "url": "/top-content-create-react-app/static/media/photosample.5f4e1a49.jpg"
+  }
+]);
